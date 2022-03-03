92e32ce85eb4ca9c168a73dd3bab9038c0f055f0
ZetoOfficial <tpptgs@bk.ru>
2022-03-03 11:18:33 +0500

    хуйню какую-то сделал

---
+++ L2Plus/Titov_Pavel_02_01.cs
@@ -0,0 +1,20 @@
+﻿namespace L2Plus
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> animals = new List<string> { "собака", "собака", "кошка", "жираф" };
+            var animalsUnique = animals.Distinct();
+            foreach (var item in animals)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            foreach (var item in animalsUnique)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
