2265ea8d7069bd8eefdeb17939c85cbe8cb5e1ca
ZetoQqq <tpptgs@bk.ru>
2022-04-17 10:19:36 +0500

    Переименовал папочку.

---
+++ L4Exmpl/Lab2/PublishingOffice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization; // нужно для сериализации объектов и сохранения в файлы
+
+namespace Lab2
+{
+    [XmlInclude(typeof(Journal)), XmlInclude(typeof(Section)), XmlInclude(typeof(Article))]
+    // указываются все дочерние классы, которые будут сериализовываться и десериализовываться
+    public class PublishingOffice
+    {
+        string publishingName;
+        public string PublishingName
+        {
+            get { return publishingName; }
+            set { publishingName = value; }
+        }
+        public PublishingOffice()
+        {
+            PublishingName = "Noname Publishing Office";
+        }
+        public PublishingOffice(string pn)
+        {
+            PublishingName = pn;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Название издательства: {0}", PublishingName);
+        }
+    }
+}
